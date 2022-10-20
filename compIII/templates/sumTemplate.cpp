3ea12fd4ccd1faaaaf30ae617d1e42cbfb010d80
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2022-10-20 11:08:45 -0400

    Sum template class
    A generic function that uses a template to some arrays of any type

---
+++ compIII/templates/sumTemplate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+T sumArray(const T arr[],const int size);
+
+
+int main(int argc, char* argv[]){
+    int arrInt[] = {1,2,3,4,5}; 
+    double arrDouble[] = {1.2,2.3,3.4};
+    float arrFloat[] = {.3, .5, .9, .8};
+    char arrChar [] = {'c', 'b', 'd'};
+    cout << "For the following input:\n";
+    cout << "int arrInt[] = {1,2,3,4,5}; \n";
+    cout << "double arrDouble[] = {1.2,2.3,3.4}; \n";
+    cout << "float arrFloat[] = {.3, .5, .9, .8};\n";
+    cout << "char arrChar [] = {'c', 'b', 'd'};\n";
+    cout << "We the output:\n";
+
+    cout << "SumInt = " << sumArray(arrInt, 5) << endl;
+    cout << "SumDouble = " << sumArray(arrDouble, 3) << endl;
+    cout << "SumFloat = " << sumArray(arrFloat, 4) << endl;
+    cout << "SumChar = " <<sumArray(arrChar, 3) << endl;
+
+    return 0;
+}
+
+template <class T>
+T sumArray(const T arr[],const int size){
+    T sum = 0;
+    for(int i = 0; i < size; i++){
+        sum += arr[i]; 
+    }
+    return sum;
+}
