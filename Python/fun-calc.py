790aec4abde55afa38f34edd8bc006df87f4ccd4
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2024-09-24 22:53:37 -0400

    inial commit

---
+++ Python/fun-calc.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+op = input("What operation would you like?")
+num1 = input("Choose num 1: ")
+num2 = input("Choose num 2: ")
+
+if op =='+':
+    print(num1 + num2)
